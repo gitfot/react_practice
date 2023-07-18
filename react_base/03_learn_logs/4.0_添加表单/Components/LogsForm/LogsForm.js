30780a14e564550d465f723dde75a48a39c47225
zs-zhanglvxiu <ptowan@163.com>
2023-07-18 12:49:25 +0800

    update

--- react_base/03_learn_logs/4.0_添加表单/Components/LogsForm/LogsForm.js
+++ react_base/03_learn_logs/4.0_添加表单/Components/LogsForm/LogsForm.js
@@ -21,15 +21,15 @@ const LogsForm = () => {
             <form onSubmit={formSubmitHandler}>
                 <div className="form-item">
                     <label htmlFor="date">日期</label>
-                    <input onChange={dateChangeHandler} id="date" type="date" name="date"/>
+                    <input onChange={dateChangeHandler} id="date" type="date" name="date" value={inputs.date || ""}/>
                 </div>
                 <div className="form-item">
                     <label htmlFor="desc">内容</label>
-                    <input onChange={dateChangeHandler} id="desc" type="text" name="desc"/>
+                    <input onChange={dateChangeHandler} id="desc" type="text" name="desc" value={inputs.desc || ""}/>
                 </div>
                 <div className="form-item">
                     <label htmlFor="time">时长</label>
-                    <input onChange={dateChangeHandler} id="time" type="number" name="time"/>
+                    <input onChange={dateChangeHandler} id="time" type="number" name="time" value={inputs.time || ""}/>
                 </div>
                 <div className="form-btn">
                     <button>添加</button>
--- react_base/03_learn_logs/4.0_添加表单/Components/LogsForm/LogsForm_old.js
+++ react_base/03_learn_logs/4.0_添加表单/Components/LogsForm/LogsForm_old.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from "../UI/Card/Card";
 import './LogsForm.css';
 
-const LogsForm_old = () => {
-    // 创建三个变量，用来存储表单中的数据
-    let inputDate = '';
-    let inputDesc = '';
-    let inputTime = 0;
+const LogsForm = () => {
+    const [inputDate, setInputDate] = useState('');
+    const [inputDesc, setInputDesc] = useState('');
+    const [inputTime, setInputTime] = useState('');
 
+    // 创建一个响应函数，监听日期的变化
     const dateChangeHandler = (e) => {
-        // event.target 执行的是触发事件的对象（DOM对象）
-        inputDate = e.target.value;
+        setInputDate(e.target.value);
     };
 
+    // 监听内容的变化
     const descChangeHandler = (e) => {
-        inputDesc = e.target.value;
+        setInputDesc(e.target.value);
     };
 
+    //监听时长的变化
     const timeChangeHandler = (e) => {
-        inputTime = e.target.value;
+        setInputTime(e.target.value);
     };
 
     const formSubmitHandler = (e) => {
-        // 取消表单的默认行为
         e.preventDefault();
-        // 获取表单项中的数据日期、内容、时长,将数据拼装为一个对象
         const newLog = {
             date: new Date(inputDate),
             desc: inputDesc,
             time: +inputTime
         };
-        window.alert(desc.value);
+
+        setInputDate('');
+        setInputDesc('');
+        setInputTime('');
+        console.log(newLog);
     };
 
     return (
@@ -38,15 +41,15 @@ const LogsForm_old = () => {
             <form onSubmit={formSubmitHandler}>
                 <div className="form-item">
                     <label htmlFor="date">日期</label>
-                    <input onChange={dateChangeHandler} id="date" type="date"/>
+                    <input onChange={dateChangeHandler} value={inputDate} id="date" type="date"/>
                 </div>
                 <div className="form-item">
                     <label htmlFor="desc">内容</label>
-                    <input onChange={descChangeHandler} id="desc" type="text"/>
+                    <input onChange={descChangeHandler} value={inputDesc} id="desc" type="text"/>
                 </div>
                 <div className="form-item">
                     <label htmlFor="time">时长</label>
-                    <input onChange={timeChangeHandler} id="time" type="number"/>
+                    <input onChange={timeChangeHandler} value={inputTime} id="time" type="number"/>
                 </div>
                 <div className="form-btn">
                     <button>添加</button>
@@ -56,4 +59,4 @@ const LogsForm_old = () => {
     );
 };
 
-export default LogsForm_old;
+export default LogsForm;
