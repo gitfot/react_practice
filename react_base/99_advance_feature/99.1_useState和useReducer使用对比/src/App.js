c733028918efa7c7336a273785e66f447868f70d
zs-zhanglvxiu <p2wan@163.com>
2023-07-24 23:41:33 +0800

    代码更新

---
+++ react_base/99_advance_feature/99.1_useState和useReducer使用对比/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UseStateTest from "./components/UseStateTest";
+import UseReducerTest from "./components/UseReducerTest";
+
+function App() {
+  return (
+    <div className="App">
+        <UseStateTest/>
+        <UseReducerTest/>
+    </div>
+  );
+}
+
+export default App;
