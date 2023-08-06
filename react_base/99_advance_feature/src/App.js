629cf81b57477fdc29fa04077f3b8db0f9f40eba
zs-zhanglvxiu <p2wan@163.com>
2023-08-07 00:26:41 +0800

    代码更新

--- react_base/99_advance_feature/src/App.js
+++ react_base/99_advance_feature/src/App.js
@@ -1,36 +1,17 @@
-import React from 'react';
-import {useAddStudentMutation, useGetStudentsQuery} from "./store/studentApi";
+import {Route} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
 
-const App = () => {
-
-    // 调用Api查询数据
-    // 这个钩子函数它会返回一个对象作为返回值，请求过程中相关数据都在该对象中存储
-    const {data, isSuccess, isGetLoading} = useGetStudentsQuery(); // 调用Api中的钩子查询数据
-    const [addStudent, { isAddLoading }] = useAddStudentMutation();
-
-    const handleAddStudent = () => {
-        addStudent({
-            data: {
-                name: 'John Doe',
-                age: 24,
-                gender: '男',
-                address:'广州'
-            }
-        })
-    }
-
+function App() {
     return (
-        <div>
-            {isGetLoading && <p>数据加载中...</p>}
-            {isSuccess && data.data.map(item => <p key={item.id}>
-                {item.attributes.name} ---
-                {item.attributes.age} ---
-                {item.attributes.gender} ---
-                {item.attributes.address}
-            </p>)}
-            <button onClick={handleAddStudent}>添加一组模拟数据</button>
+        <div className="App">
+            <Menu/>
+
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
         </div>
     );
-};
+}
 
 export default App;
